e74a63e857b19532f5dfe3a5f92896cf0f315831
MajorDong <odsts@outlook.com>
2020-02-20 10:45:20 +0800

    完成页面效果

--- index.js
+++ index.js
@@ -65,5 +65,40 @@ filterBtns.forEach((btn)=>{
 })
 
 
+const staggeringOption = {
+  delay: 300,
+  distance: '50px',
+  easing: 'ease-in-out',
+  origin: 'bottom',
+}
+ScrollReveal().reveal('.feature',{...staggeringOption, interval: 350})
+ScrollReveal().reveal('.service-item',{...staggeringOption, interval: 350})
 
+const dataEl = document.querySelector('.data')
+function bgPositionContr(bottom){
+  let bottomDi = bottom / 7
+  dataEl.style.backgroundPosition = `center calc(50% - ${bottomDi}px)`
+}
+ScrollReveal().reveal('.data',{
+  beforeReveal: () => {
+    anime({
+      targets: '.data-piece .num',
+      innerHTML: (el) => {
+        return [0, el.innerHTML]
+      },
+      duration: 1000,
+      round: 1,
+      easing: 'easeInExpo'
+    })
+    let { bottom } = dataEl.getBoundingClientRect()
+    bgPositionContr(bottom)
+  }
+})
 
+window.addEventListener('scroll',()=>{
+  let { top, bottom } = dataEl.getBoundingClientRect()
+  //判断是否在可见区域
+  if(bottom > 0 && top < window.innerHeight){
+    bgPositionContr(bottom)
+  }
+})
